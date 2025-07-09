818fc76263c9e8b144c65c0c49e145c779e7ba52
nozoeyuzu <citron12yz21n82@docomo.ne.jp>
2025-07-09 21:50:02 +0900

    best_fit

--- week7/best_fit_malloc.c
+++ week7/best_fit_malloc.c
@@ -25,54 +25,54 @@ void munmap_to_system(void *ptr, size_t size);
 //      the size of the metadata.
 //   *  The free slots are linked with a singly linked list (we call this a
 //      free list). |next| points to the next free slot.
-typedef struct simple_metadata_t {
+typedef struct my_metadata_t {
   size_t size;
-  struct simple_metadata_t *next;
-} simple_metadata_t;
+  struct my_metadata_t *next;
+} my_metadata_t;
 
-// The global information of the simple malloc.
+// The global information of the my malloc.
 //   *  |free_head| points to the first free slot.
 //   *  |dummy| is a dummy free slot (only used to make the free list
-//      implementation simpler).
-typedef struct simple_heap_t {
-  simple_metadata_t *free_head;
-  simple_metadata_t dummy;
-} simple_heap_t;
+//      implementation myr).
+typedef struct my_heap_t {
+  my_metadata_t *free_head;
+  my_metadata_t dummy;
+} my_heap_t;
 
-simple_heap_t simple_heap;
+my_heap_t my_heap;
 
 // Add a free slot to the beginning of the free list.
-void simple_add_to_free_list(simple_metadata_t *metadata) {
+void my_add_to_free_list(my_metadata_t *metadata) {
   assert(!metadata->next);
-  metadata->next = simple_heap.free_head;
-  simple_heap.free_head = metadata;
+  metadata->next = my_heap.free_head;
+  my_heap.free_head = metadata;
 }
 
 // Remove a free slot from the free list.
-void simple_remove_from_free_list(simple_metadata_t *metadata,
-                                  simple_metadata_t *prev) {
+void my_remove_from_free_list(my_metadata_t *metadata,
+                                  my_metadata_t *prev) {
   if (prev) {
     prev->next = metadata->next;
   } else {
-    simple_heap.free_head = metadata->next;
+    my_heap.free_head = metadata->next;
   }
   metadata->next = NULL;
 }
 
 // This is called at the beginning of each challenge.
-void simple_initialize() {
-  simple_heap.free_head = &simple_heap.dummy;
-  simple_heap.dummy.size = 0;
-  simple_heap.dummy.next = NULL;
+void my_initialize() {
+  my_heap.free_head = &my_heap.dummy;
+  my_heap.dummy.size = 0;
+  my_heap.dummy.next = NULL;
 }
 
-void *simple_malloc(size_t size) {
-  simple_metadata_t *meta = simple_heap.free_head;
-  simple_metadata_t *prev = NULL;
+void *my_malloc(size_t size) {
+  my_metadata_t *meta = my_heap.free_head;
+  my_metadata_t *prev = NULL;
 
   // --- Best-fit 用の変数を用意 ---
-  simple_metadata_t *best_meta = NULL;
-  simple_metadata_t *best_prev = NULL;
+  my_metadata_t *best_meta = NULL;
+  my_metadata_t *best_prev = NULL;
   size_t best_remainder = SIZE_MAX;
 
   // 空きリスト全体をスキャン
@@ -94,26 +94,26 @@ void *simple_malloc(size_t size) {
   // ベストが見つからなかったら mmap してリトライ
   if (!best_meta) {
     size_t buffer_size = 4096;
-    simple_metadata_t *new_meta =
-      (simple_metadata_t*)mmap_from_system(buffer_size);
-    new_meta->size = buffer_size - sizeof(simple_metadata_t);
+    my_metadata_t *new_meta =
+      (my_metadata_t*)mmap_from_system(buffer_size);
+    new_meta->size = buffer_size - sizeof(my_metadata_t);
     new_meta->next = NULL;
-    simple_add_to_free_list(new_meta);
-    return simple_malloc(size);
+    my_add_to_free_list(new_meta);
+    return my_malloc(size);
   }
 
   // best_meta／best_prev を使って切り出し
   void *ptr = best_meta + 1;
   size_t remaining_size = best_meta->size - size;
-  simple_remove_from_free_list(best_meta, best_prev);
+  my_remove_from_free_list(best_meta, best_prev);
 
-  if (remaining_size > sizeof(simple_metadata_t)) {
+  if (remaining_size > sizeof(my_metadata_t)) {
     best_meta->size = size;
-    simple_metadata_t *new_meta =
-      (simple_metadata_t*)((char*)ptr + size);
-    new_meta->size = remaining_size - sizeof(simple_metadata_t);
+    my_metadata_t *new_meta =
+      (my_metadata_t*)((char*)ptr + size);
+    new_meta->size = remaining_size - sizeof(my_metadata_t);
     new_meta->next = NULL;
-    simple_add_to_free_list(new_meta);
+    my_add_to_free_list(new_meta);
   }
   return ptr;
 }
